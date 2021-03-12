757c4b8e96ee63da46a14a7c39b52ab720a47216
Yuunan Kin <yuunan.kin@gmail.com>
2021-03-12 09:05:49 +0900

    delete song mutaion & import css in react

--- frontend/components/SongList.js
+++ frontend/components/SongList.js
@@ -1,18 +1,39 @@
 import React from "react";
-import { gql, useQuery } from "@apollo/client";
+import { gql, useMutation, useQuery } from "@apollo/client";
 import { Link } from "react-router-dom";
 import { fetchSongs } from "../queries/fetch-songs";
+const deleteSongMutation = gql`
+  mutation deleteSong($id: ID!) {
+    deleteSong(id: $id) {
+      id
+    }
+  }
+`;
 const SongList = () => {
   const { loading, error, data } = useQuery(fetchSongs);
-
+  const [deleteSong, { deleteSongData }] = useMutation(deleteSongMutation);
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :</p>;
+  const onDelete = async (id, title) => {
+    if (confirm(`Delete song ${title}?`)) {
+      await deleteSong({
+        variables: { id },
+        refetchQueries: [{ query: fetchSongs }],
+      });
+    }
+  };
 
   return (
     <ul className="collection">
       {data.songs.map((song) => (
         <li key={song.id} className="collection-item">
           {song.title}
+          <i
+            className="material-icons"
+            onClick={(e) => onDelete(song.id, song.title)}
+          >
+            delete
+          </i>
         </li>
       ))}
       <Link to="/song-create" className="collection-item">
--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,4 @@
+import "./style/style.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Router, Route, Switch, BrowserRouter } from "react-router-dom";
