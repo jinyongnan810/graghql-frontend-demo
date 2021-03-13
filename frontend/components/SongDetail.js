9219b4fe2332814d6b17c36141da3ac7b8708f5b
Yuunan Kin <yuunan.kin@gmail.com>
2021-03-14 07:54:17 +0900

    add lyric to a song

--- frontend/components/SongDetail.js
+++ frontend/components/SongDetail.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { gql, useMutation, useQuery } from "@apollo/client";
 import { useParams } from "react-router";
-const fetchSongs = gql`
+const fetchSong = gql`
   query Song($id: ID!) {
     song(id: $id) {
       id
@@ -14,9 +15,19 @@ const fetchSongs = gql`
     }
   }
 `;
+
+const createLyricMutation = gql`
+  mutation addLyricToSong($content: String!, $songId: ID!) {
+    addLyricToSong(content: $content, songId: $songId) {
+      id
+    }
+  }
+`;
 const SongDetail = () => {
   const { id } = useParams();
-  const { loading, error, data } = useQuery(fetchSongs, { variables: { id } });
+  const { loading, error, data } = useQuery(fetchSong, { variables: { id } });
+  const [newLyric, setNewLyric] = useState("");
+  const [addLyricToSong, { addLyricData }] = useMutation(createLyricMutation);
   if (error) {
     return <h4>{error}</h4>;
   }
@@ -35,12 +46,31 @@ const SongDetail = () => {
       </ul>
     </div>
   );
+  const onCreateLyric = async (e) => {
+    e.preventDefault();
+    if (newLyric.trim()) {
+      await addLyricToSong({
+        variables: { content: newLyric.trim(), songId: data.song.id },
+        refetchQueries: [{ query: fetchSong, variables: { id } }],
+      });
+      setNewLyric("");
+    }
+  };
   return (
     <div>
       <h3>Song Detail</h3>
       <hr />
+      <Link to="/">Back</Link>
       {display}
-      <button>Create Lyrics</button>
+      <form onSubmit={onCreateLyric}>
+        <input
+          type="text"
+          placeholder="Add a lyric!"
+          value={newLyric}
+          onChange={(e) => setNewLyric(e.target.value)}
+        />
+        <button>Create Lyrics</button>
+      </form>
     </div>
   );
 };
